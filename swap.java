d0aeb3d78d1ea09874d549c7ffb7ac8f7e24132f
Vishal0-ai <vishalyy1001@gmail.com>
2025-03-11 17:35:14 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,35 @@
+public class swap {
+    public static void main(String[] args) {
+        int n=10;
+        int m=20;
+        /* By using temp variable
+        int temp=0;
+        temp=m;
+        m=n;
+        n=temp;  
+        */
+
+        /* By using multiple and divid
+        n=n*m;
+        m=n/m;
+        n=n/m;
+        */
+
+        /* By using add and sub
+        n=n+m;
+        m=n-m;
+        n=n-m;
+        */
+        /*  Bitwise
+        n=n^m;
+        m=n^m;
+        n=n^m;
+        */
+
+
+        System.out.println(n);
+        System.out.print(m);
+
+    }
+    
+}
